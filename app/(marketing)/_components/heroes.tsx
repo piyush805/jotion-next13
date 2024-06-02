767fcd2e874403b9d1bb91a5aacd7801dbf7de25
Piyush <pshtiwari0@gmail.com>
2024-06-02 20:00:05 +0530

    initial commit - folder setup, landing page, clerk auth

---
+++ app/(marketing)/_components/heroes.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export const Heroes = () => {
+  return (
+    <div className="flex flex-col items-center justify-center max-w-5xl dark:bg-[#1F1F1F]">
+      <div className="flex items-center">
+        <div className="relative w-[300px] h-[300px] sm:h-[350px] md:h-[400px] md:w-[400px]">
+          <Image
+            src="/hero-1.jpg"
+            fill
+            className="object-contain"
+            alt="Documents"
+          />
+        </div>
+        <div className="relative w-[400px] h-[400px] hidden md:block ">
+          <Image
+            src="/hero-1.jpg"
+            fill
+            className="object-contain"
+            alt="Documents"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
